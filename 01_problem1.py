05ca841ad8a509c8cf99b8189ee4e879100b8560
bagadeanushka07-a11y <bagadeanushka07@gmail.com>
2026-04-07 23:02:47 +0530

    Add files via upload
    Write a python program to add two numbers.

---
+++ 01_problem1.py
@@ -0,0 +1,9 @@
+# Write a python program to add two numbers.
+a = int(input("Enter number1:"))
+
+b = int(input("Enter number2:"))
+
+print("number a is:",a)
+print("number b is:",b)
+
+print("Sum is", a+b)
